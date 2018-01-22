3a7e84b525d5e946b132bd12073bb8ce79fc0c13
ellisonc <ellisonc@wustl.edu>
2018-01-21 22:36:26 -0600

    Follow tutorial for basic api

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.get('/status', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+  res.send({
+    message: `${req.body.email} is registered`
+  })
+})
+
 app.listen(process.env.PORT || 3000)
